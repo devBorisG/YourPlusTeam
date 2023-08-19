fa4532c5aa337ffa27e15fe55411d70955774b68
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-08-18 20:25:04 -0500

    add last projects

---
+++ repositoryYourPlus/src/main/java/com/uco/yourplus/repositoryYourPlus/RepositoryYourPlusApplication.java
@@ -0,0 +1,13 @@
+package com.uco.yourplus.repositoryYourPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryYourPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryYourPlusApplication.class, args);
+	}
+
+}
---
+++ repositoryYourPlus/src/test/java/com/uco/yourplus/repositoryYourPlus/RepositoryYourPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.yourplus.repositoryYourPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryYourPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
