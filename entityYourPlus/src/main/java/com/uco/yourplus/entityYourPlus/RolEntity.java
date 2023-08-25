492f1ef1b9fb8546c54e9bba95a24e2483a67576
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2023-08-24 19:25:59 -0500

    Entity Rol

--- entityYourPlus/src/main/java/com/uco/yourplus/entityYourPlus/RolEntity.java
+++ entityYourPlus/src/main/java/com/uco/yourplus/entityYourPlus/RolEntity.java
@@ -1,4 +1,22 @@
 package com.uco.yourplus.entityYourPlus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Rol")
 public class RolEntity {
+    @Id
+    UUID id;
+    String descripcion;
+
 }
