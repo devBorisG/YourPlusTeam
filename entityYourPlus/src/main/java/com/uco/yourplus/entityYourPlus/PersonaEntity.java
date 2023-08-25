87f2f5349c6f7ea8a9b9c1b9c76aab898b68f73c
nrl0315 <nicolas.rendon5948@soyuco.edu.co>
2023-08-24 19:28:27 -0500

    Creacion de Persona Entity

--- entityYourPlus/src/main/java/com/uco/yourplus/entityYourPlus/PersonaEntity.java
+++ entityYourPlus/src/main/java/com/uco/yourplus/entityYourPlus/PersonaEntity.java
@@ -1,4 +1,25 @@
 package com.uco.yourplus.entityYourPlus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Usuario")
 public class PersonaEntity {
+    @Id
+    UUID id;
+    String nombre;
+    String apellido;
+    String correo;
+    String password;
+    @ManyToOne
+    @JoinColumn(name = "rol_id")
+    RolEntity rolEntity;
 }
