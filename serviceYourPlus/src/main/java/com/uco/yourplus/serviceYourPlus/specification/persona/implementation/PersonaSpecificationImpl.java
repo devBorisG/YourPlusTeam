d10b2830364b61f4204e670f66eca45300688160
nrl0315 <nicolas.rendon5948@soyuco.edu.co>
2023-09-02 11:37:07 -0500

    Creacion de las especificaciones y los casos de uso para persona y rol

--- serviceYourPlus/src/main/java/com/uco/yourplus/serviceYourPlus/specification/persona/implementation/PersonaSpecificationImpl.java
+++ serviceYourPlus/src/main/java/com/uco/yourplus/serviceYourPlus/specification/persona/implementation/PersonaSpecificationImpl.java
@@ -1,13 +1,47 @@
 package com.uco.yourplus.serviceYourPlus.specification.persona.implementation;
 
+import com.uco.yourplus.crosscuttingYourPlus.exceptions.service.ServiceCustomException;
+import com.uco.yourplus.repositoryYourPlus.persona.PersonaRepository;
+import com.uco.yourplus.repositoryYourPlus.rol.RolRepository;
 import com.uco.yourplus.serviceYourPlus.domain.PersonaDomain;
+import com.uco.yourplus.serviceYourPlus.domain.RolDomain;
 import com.uco.yourplus.serviceYourPlus.specification.persona.PersonaSpecification;
+import com.uco.yourplus.serviceYourPlus.usecase.rol.ConsultarRol;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.uco.yourplus.crosscuttingYourPlus.helper.StringHelper.isEmpty;
+import static com.uco.yourplus.crosscuttingYourPlus.helper.UUIDHelper.isDefaultUUID;
 
 @Service
 public class PersonaSpecificationImpl implements PersonaSpecification {
+
+    @Autowired
+    PersonaRepository repository;
+
+    @Autowired
+    RolRepository rolRepository;
+
+    @Autowired
+    ConsultarRol consultarRol;
+
     @Override
     public void isSatisfied(PersonaDomain data) {
 
+    }
+
+    private void verifyUserIntegrity(PersonaDomain personaDomain){
+        if(repository.findById(personaDomain.getId()).isPresent());{
+            throw ServiceCustomException.createUserException("El usuario ya existe");
+        }
+    }
+
+    private void verifyRolIngegrity(RolDomain rolDomain){
+
+    }
+
+    private boolean verifyMandatoryPersonaAttributes(PersonaDomain personaDomain){
+        return isDefaultUUID(personaDomain.getId()) || isEmpty(personaDomain.getNombre()) || isEmpty(personaDomain.getApellido()) ||
+                isEmpty(personaDomain.getCorreo()) || isEmpty(personaDomain.getPassword());
     }
 }
--- serviceYourPlus/src/main/java/com/uco/yourplus/serviceYourPlus/usecase/rol/implementation/ConsultarRolImpl.java
+++ serviceYourPlus/src/main/java/com/uco/yourplus/serviceYourPlus/usecase/rol/implementation/ConsultarRolImpl.java
@@ -1,13 +1,17 @@
 package com.uco.yourplus.serviceYourPlus.usecase.rol.implementation;
 
+import com.uco.yourplus.entityYourPlus.RolEntity;
 import com.uco.yourplus.repositoryYourPlus.rol.RolRepository;
 import com.uco.yourplus.serviceYourPlus.domain.RolDomain;
 import com.uco.yourplus.serviceYourPlus.usecase.rol.ConsultarRol;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ConsultarRolImpl implements ConsultarRol {
@@ -16,6 +20,19 @@ public class ConsultarRolImpl implements ConsultarRol {
     private RolRepository rolRepository;
     @Override
     public List<RolDomain> execute(Optional<RolDomain> domain) {
-        return null;
+        List<RolEntity> result;
+        List<RolDomain> castResult = new ArrayList<>();
+        try{
+            result = rolRepository.findAll();
+        } catch(Exception e){
+            throw e;
+        }
+        result.stream().map(value -> {
+            RolDomain rolDomain = new RolDomain();
+            BeanUtils.copyProperties(value,rolDomain);
+            return rolDomain;
+        }).forEach(castResult::add);
+
+        return castResult;
     }
 }
