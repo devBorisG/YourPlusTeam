b807633b97071a0b16bc392f2b73e08fa304f04c
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-09-02 12:47:09 -0500

    Caso de uso consultar Roles

---
+++ serviceYourPlus/src/main/java/com/uco/yourplus/serviceYourPlus/usecase/rol/ConsultarRoles.java
@@ -0,0 +1,10 @@
+package com.uco.yourplus.serviceYourPlus.usecase.rol;
+
+import com.uco.yourplus.serviceYourPlus.domain.RolDomain;
+import com.uco.yourplus.serviceYourPlus.usecase.UseCaseList;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ConsultarRoles extends UseCaseList<RolDomain> {
+}
