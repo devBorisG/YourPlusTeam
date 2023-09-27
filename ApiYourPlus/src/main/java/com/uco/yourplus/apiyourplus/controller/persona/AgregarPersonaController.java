fb55610d5af82f6edd0404337cc9dc8f0de208f3
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-09-26 19:25:53 -0500

    Correcion de errores mapping ambiguo y falta de dependencias jakson

--- ApiYourPlus/src/main/java/com/uco/yourplus/apiyourplus/controller/persona/AgregarPersonaController.java
+++ ApiYourPlus/src/main/java/com/uco/yourplus/apiyourplus/controller/persona/AgregarPersonaController.java
@@ -3,7 +3,6 @@ package com.uco.yourplus.apiyourplus.controller.persona;
 import com.uco.yourplus.apiyourplus.controller.response.Response;
 import com.uco.yourplus.crosscuttingyourplus.exceptions.YourPlusCustomException;
 import com.uco.yourplus.dtoyourplus.builder.PersonaDTO;
-import com.uco.yourplus.serviceyourplus.authentication.AuthenticationGeneric;
 import com.uco.yourplus.serviceyourplus.facade.persona.RegistrarPersonaFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,9 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/apiyourplus/persona")
 public class AgregarPersonaController {
-
-    @Autowired
-    AuthenticationGeneric authentication;
 
     @Autowired
     RegistrarPersonaFacade registrarPersonaFacade;
@@ -48,21 +44,6 @@ public class AgregarPersonaController {
         } catch(final Exception exception){
             httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
             response.addFatalMessage("apague y vuelva a prender que c murio el sistema");
-        }
-        return new ResponseEntity<>(response,httpStatus);
-    }
-
-    @PostMapping("/authenticate")
-    public ResponseEntity<Response<PersonaDTO>> authenticate(@RequestBody PersonaDTO personaDTO) {
-        final Response<PersonaDTO> response = new Response<>();
-        HttpStatus httpStatus = HttpStatus.OK;
-        try{
-            String jwtToken = authentication.authenticate(personaDTO);
-            response.setToken(jwtToken);
-            response.addSuccesMessage("Persona autenticada");
-        }catch (Exception exception){
-            httpStatus = HttpStatus.BAD_REQUEST;
-            response.addErrorMessage("Persona no autenticada");
         }
         return new ResponseEntity<>(response,httpStatus);
     }
