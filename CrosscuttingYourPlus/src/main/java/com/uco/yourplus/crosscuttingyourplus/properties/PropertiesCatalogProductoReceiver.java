b568ff33577bcb77ff972cfc974de00e6061aa92
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-10-09 00:19:39 -0500

    Development of logic to receive the response from the microservice processor

--- CrosscuttingYourPlus/src/main/java/com/uco/yourplus/crosscuttingyourplus/properties/PropertiesCatalogProductoReceiver.java
+++ CrosscuttingYourPlus/src/main/java/com/uco/yourplus/crosscuttingyourplus/properties/PropertiesCatalogProductoReceiver.java
@@ -1,10 +1,8 @@
 package com.uco.yourplus.crosscuttingyourplus.properties;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Data
 @Component
 @ConfigurationProperties(prefix = "yourplus.management.producto.response")
 public class PropertiesCatalogProductoReceiver {
@@ -14,19 +12,105 @@ public class PropertiesCatalogProductoReceiver {
 
     private RoutingKeyProperties routingkey = new RoutingKeyProperties();
 
-    @Data
+    public String getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+    public QueueProperties getQueue() {
+        return queue;
+    }
+
+    public void setQueue(QueueProperties queue) {
+        this.queue = queue;
+    }
+
+    public RoutingKeyProperties getRoutingkey() {
+        return routingkey;
+    }
+
+    public void setRoutingkey(RoutingKeyProperties routingkey) {
+        this.routingkey = routingkey;
+    }
+
     public static class QueueProperties {
         private String save;
         private String delete;
         private String update;
         private String list;
+
+        public String getSave() {
+            return save;
+        }
+
+        public void setSave(String save) {
+            this.save = save;
+        }
+
+        public String getDelete() {
+            return delete;
+        }
+
+        public void setDelete(String delete) {
+            this.delete = delete;
+        }
+
+        public String getUpdate() {
+            return update;
+        }
+
+        public void setUpdate(String update) {
+            this.update = update;
+        }
+
+        public String getList() {
+            return list;
+        }
+
+        public void setList(String list) {
+            this.list = list;
+        }
     }
 
-    @Data
     public static class RoutingKeyProperties {
         private String save;
         private String delete;
         private String update;
         private String list;
+
+        public String getSave() {
+            return save;
+        }
+
+        public void setSave(String save) {
+            this.save = save;
+        }
+
+        public String getDelete() {
+            return delete;
+        }
+
+        public void setDelete(String delete) {
+            this.delete = delete;
+        }
+
+        public String getUpdate() {
+            return update;
+        }
+
+        public void setUpdate(String update) {
+            this.update = update;
+        }
+
+        public String getList() {
+            return list;
+        }
+
+        public void setList(String list) {
+            this.list = list;
+        }
     }
 }
---
+++ ServiceYourPlus/src/main/java/com/uco/yourplus/serviceyourplus/response/microservices/procesador/HandlerReceiveMessageSave.java
@@ -0,0 +1,6 @@
+package com.uco.yourplus.serviceyourplus.response.microservices.procesador;
+
+import com.uco.yourplus.serviceyourplus.response.HandlerReceiveMessage;
+
+public interface HandlerReceiveMessageSave extends HandlerReceiveMessage {
+}
